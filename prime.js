47312f329db43396dbdd1d7fb452887a70e7633d
chaudhari014 <fatarambhai.k14@gmail.com>
2023-02-23 00:40:36 +0530

    ok done check

--- prime.js
+++ prime.js
@@ -12,3 +12,21 @@ if(count==0){
     console.log(n,"number is not prime")
   }
 }
+
+
+// copy
+
+function primenumber(n){
+  let count=0;
+  for(let i=2;i<Math.sqrt(n);i++){
+    if(n%i==0){
+      count++;
+    }
+  }
+if(count==0){
+    console.log(n,"prime number")
+  }
+  else{
+    console.log(n,"number is not prime")
+  }
+}
